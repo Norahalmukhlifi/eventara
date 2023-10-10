ceb76fe81457e814cd399075e8bd1244a95989bc
Norahalmukhlifi <norahalmokhlifi@gmail.com>
2023-10-10 09:28:42 +0300

    Initial Commit

---
+++ eventara/eventaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  eventaraApp.swift
+//  eventara
+//
+//  Created by NorahAlmukhlifi on 25/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct eventaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
