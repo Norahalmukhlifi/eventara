7115b532f5488bde673601f3147d6293680d87b4
reembojeer <reembojeer@gmail.com>
2023-10-16 12:57:54 +0300

    logoo

--- eventara/splash.swift
+++ eventara/splash.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct splash: View {
+    @State private var isActive = false
+    @State private var logoScale: CGFloat = 1.0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color("Back")
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                Image("logo") // Replace "your_logo_name" with the actual image name.
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200)
+                    .scaleEffect(logoScale) // Apply the scale effect
+                    .onAppear {
+                        withAnimation(Animation.easeInOut(duration: 2)) {
+                            logoScale = 1.2 // Increase the scale to zoom in
+                        }
+                    }
+                
+                
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                }
+            }
+            .background(NavigationLink("", destination: MainPage(), isActive: $isActive))
+        }
     }
 }
-
 #Preview {
     splash()
 }
